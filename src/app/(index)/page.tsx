a7ffbfa9fb47dd35821143eaaf9f5b2c07c83608
Keita Ito <eguchi@eguchi.cc>
2024-11-12 01:47:01 +0900

    feat: safe-areaのテスト

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,3 +1,53 @@
+"use client";
+
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import RestoreIcon from "@mui/icons-material/Restore";
+
 export default function IndexPage() {
-  return <div>Index Page</div>;
+  const [value, setValue] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Typography variant="h1">Hello, World!</Typography>
+        <Typography variant="h1">Hello, World!</Typography>
+      </div>
+      <Paper
+        elevation={3}
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      >
+        {/* ここで調整してる！！！ */}
+        <Box pb="env(safe-area-inset-bottom)" sx={{ bgcolor: "red" }}>
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(_event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+          </BottomNavigation>
+        </Box>
+      </Paper>
+    </>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,35 @@
+import { theme } from "@/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { Roboto } from "next/font/google";
+
 import type { PropsWithChildren } from "react";
+import type { Viewport } from "next";
 
 import "./globals.css";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+export const viewport: Viewport = {
+  viewportFit: "cover",
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className={roboto.variable}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
